af1517307099594c022bb3edf2fe80f680bc8f46
Adam Faryna <adamfaryna@gmail.com>
2017-04-29 20:28:05 +0100

    Removed AOL specific JSX template.

--- vim/vim/templates/aol/=template=.jsx
+++ vim/vim/templates/aol/=template=.jsx
@@ -1,16 +1,1 @@
 // Copyright (C) 2017 AOL Platforms
-import React, { Component } from 'react'
-
-
-class =template= extends Component {
-  constructor(props: any) {
-    super(props)
-
-  }
-
-  render() {
-    return (
-
-    )
-  }
-}
