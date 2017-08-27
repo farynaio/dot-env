27952ef093be320cc5db2c35dfc3594b2412d095
Adam Faryna <adamfaryna@gmail.com>
2017-08-28 00:09:36 +0700

    VIM: added private template for PHP files

---
+++ vim/vim/templates/private/==template=.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * Short description for
+  *
+  * @package package
+  * @author devil <adamfaryna@appdy.net>
+  * @version 0.1
+  * @copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
+  * @license Proprietary
+  */
+?>
