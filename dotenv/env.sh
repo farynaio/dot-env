ed6923eb15449e105b5b93875bb20bd8feff6237
Adam Faryna <adamfaryna@gmail.com>
2017-09-17 16:31:51 +0100

    TMUX & ENV: fix tmux aliases and docker env init errors.

--- dotenv/env.sh
+++ dotenv/env.sh
@@ -3,6 +3,6 @@
 source $HOME/.dotenv/exports.sh
 source $HOME/.dotenv/aliases.sh
 
-eval "$(docker-machine env default)"
+eval "$(docker-machine env default &>/dev/null)"
 
 source $(brew --prefix php-version)/php-version.sh && php-version 5
