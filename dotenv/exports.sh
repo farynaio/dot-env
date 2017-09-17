1414fda3c79db033faa4d4e815602cc68f47f864
Adam Faryna <adamfaryna@gmail.com>
2017-09-17 16:32:36 +0100

    SH: swiched to brew android-sdk.

--- dotenv/exports.sh
+++ dotenv/exports.sh
@@ -6,17 +6,19 @@ export USERNAME="Adam Faryna"
 export EMAIL="adamfaryna@appdy.net"
 
 #export ANDROID_HOME=/usr/local/opt/android-sdk
-export ANDROID_HOME=/Users/devil/Development/Java/android-sdk-macosx
+# export ANDROID_HOME=/Users/devil/Development/Java/android-sdk-macosx
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
 #export STUDIO_JDK=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk
 export JAVA_HOME=$(/usr/libexec/java_home)
 export EDITOR="/usr/bin/vim"
-export PATH=/usr/local/bin:/Users/devil/Development/Java/android-sdk-macosx/tools:$PATH
+# export PATH=/usr/local/bin:/Users/devil/Development/Java/android-sdk-macosx/tools:$PATH
 export SED=`which sed`
 
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export PATH=/Users/devil/Development/codenameone/codenameone-build-tools/tools/codenameone-readonly/Ports/iOSPort/xmlvm/install/bin:$PATH
-export PATH=/Users/devil/Development/Java/android-sdk-macosx/platform-tools:$PATH
+# export PATH=/Users/devil/Development/codenameone/codenameone-build-tools/tools/codenameone-readonly/Ports/iOSPort/xmlvm/install/bin:$PATH
+# export PATH=/Users/devil/Development/Java/android-sdk-macosx/platform-tools:$PATH
+export PATH=/usr/local/share/android-sdk/tools/bin:$PATH
 export PATH="/usr/local/mysql/bin:/usr/local/mysql/bin/mysqladmin:$PATH"
 export PATH="/Users/devil/bin:$PATH"
 export PATH="$HOME/.npm-packages/bin:$PATH"
