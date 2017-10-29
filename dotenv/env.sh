749d3d0bb0509252bd14b0e9bcead6547db507a5
Adam Faryna <adamfaryna@appdy.net>
2017-10-29 14:00:30 +0000

    SH: moved rbenv config from bash to general shell config

--- dotenv/env.sh
+++ dotenv/env.sh
@@ -5,4 +5,6 @@ source $HOME/.dotenv/aliases.sh
 
 eval "$(docker-machine env default &>/dev/null)"
 
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
 source $(brew --prefix php-version)/php-version.sh && php-version 5
