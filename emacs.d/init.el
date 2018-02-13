404d61b01ec179bc73e695658d81fa6602c7b488
Adam Faryna <adamfaryna@appdy.net>
2018-02-13 00:07:12 +0000

    emacs: switched to darker mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -476,40 +476,10 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(ansi-color-faces-vector
-     [default bold shadow italic underline bold bold-italic bold])
-  '(ansi-color-names-vector
-     (vector "#ffffff" "#ff9da4" "#d1f1a9" "#ffeead" "#bbdaff" "#ebbbff" "#99ffff" "#003f8e"))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
   '(custom-safe-themes
      (quote
-       ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
- '(fci-rule-color "#003f8e")
-  '(package-selected-packages
-     (quote
-       (wgrep smex flx-ido ido-better-flex ido-completing-read+ ido-ubiquitous-mode rainbow-mode persistent-scratch neotree multiple-cursors hl-todo helm git-gutter editorconfig color-theme-sanityinc-tomorrow centered-cursor-mode auto-highlight-symbol)))
- '(vc-annotate-background nil)
-  '(vc-annotate-color-map
-     (quote
-       ((20 . "#ff9da4")
-         (40 . "#ffc58f")
-         (60 . "#ffeead")
-         (80 . "#d1f1a9")
-         (100 . "#99ffff")
-         (120 . "#bbdaff")
-         (140 . "#ebbbff")
-         (160 . "#ff9da4")
-         (180 . "#ffc58f")
-         (200 . "#ffeead")
-         (220 . "#d1f1a9")
-         (240 . "#99ffff")
-         (260 . "#bbdaff")
-         (280 . "#ebbbff")
-         (300 . "#ff9da4")
-         (320 . "#ffc58f")
-         (340 . "#ffeead")
-         (360 . "#d1f1a9"))))
- '(vc-annotate-very-old-color nil))
+       ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
