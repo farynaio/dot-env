645cbce3b329ce0acefbd4d76a4904fcca11a053
Adam Faryna <adamfaryna@appdy.net>
2018-05-29 10:38:24 +0100

    emacs: added some abbreviations

--- emacs.d/modules/my-writing.el
+++ emacs.d/modules/my-writing.el
@@ -2,6 +2,10 @@
 (setq save-abbrevs 'silently)
 
 (define-abbrev-table 'global-abbrev-table '(
-  ("im" "I'm" nil 0)))
+                                             ("im" "I'm" nil 0)
+                                             ("dont" "don't" nil 0)
+                                             ("cant" "can't" nil 0)
+                                             ("shouldnt" "shouldn't" nil 0)
+                                             ))
 
 (provide 'my-writing)
