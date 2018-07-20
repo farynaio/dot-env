f03b281092be89df913583fcfc0b409c4ff666f3
Adam Faryna <adamfaryna@appdy.net>
2018-07-20 15:57:55 +0100

    emacs: added company-emoji
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 8 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   emacs.d/init.el
    #
    # Changes not staged for commit:
    #	modified:   aspell/aspell.en.pws
    #	modified:   dotenv/exports.sh
    #	modified:   dotenv/functions.sh
    #	modified:   emacs.d/modules/my-dired.el
    #	modified:   emacs.d/modules/my-email.el
    #	modified:   emacs.d/modules/my-org.el
    #	modified:   gitconfig
    #	modified:   iterm2/com.googlecode.iterm2.plist
    #
    # Untracked files:
    #	scripts/backup.sh
    #

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -369,13 +369,38 @@
   '((ispell-dictionary . "en")
      (ispell-dictionary . "pl")))
 
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package company-emoji
+  :config
+  (progn
+    (add-to-list 'company-backends 'company-emoji)))
+
+(defun my/set-emoji-font (frame)
+  "Adjust the font settings of FRAME so Emacs can display emoji properly."
+  (if (eq system-type 'darwin)
+      ;; For NS/Cocoa
+      (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)
+    ;; For Linux
+    (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'prepend)))
+
+;; For when Emacs is started in GUI mode:
+(my/set-emoji-font nil)
+
+;; Hook for when a frame is created with emacsclient
+;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
+(add-hook 'after-make-frame-functions 'my/set-emoji-font)
+
 (display-time-mode -1)
-(setq debug-on-error nil)
+;; (setq debug-on-error nil)
 
 ;; nice themes
 ;; deeper-blue
 ;; tango dark
 ;; wombat
+
+;; light theme
+;; adwaita
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
