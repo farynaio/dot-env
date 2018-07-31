2ebd38dfb9b012a27468c5a35290bb0343dd788b
Adam Faryna <adamfaryna@appdy.net>
2018-07-31 22:15:04 +0100

    emacs: better region face background for wombat
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   emacs.d/init.el
    #
    # Changes not staged for commit:
    #	modified:   emacs.d/init.el
    #	modified:   emacs.d/modules/my-email.el
    #	modified:   emacs.d/modules/my-org.el
    #	modified:   gitconfig
    #
    # Untracked files:
    #	emacs.d/recentf~
    #	emacs.d/themes/wombat-theme_ee.el
    #	scripts/backup.sh
    #

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -426,6 +426,7 @@
   (custom-theme-set-faces
     'wombat
     '(cursor ((t (:inherit nil :underline nil :background "White"))))
+    '(region ((t (:inherit nil :underline nil :foreground "White" :background "RoyalBlue4"))))
     '(hl-line ((t (:inherit nil :underline nil :background "gray34"))))
     '(ledger-font-xact-highlight-face ((t nil)))
     '(org-level-2 ((t (:inherit outline-2 :foreground "cyan2"))))
