097c3e208f586e11af3c0248beb112a453543b6d
Adam Faryna <adamfaryna@appdy.net>
2018-08-09 15:29:35 +0100

    emacs: replaced ibuffer shortcut which pop-to-buffer
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 2 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   emacs.d/modules/my-navigation.el
    #
    # Changes not staged for commit:
    #	modified:   emacs.d/modules/my-org.el
    #
    # Untracked files:
    #	emacs.d/recentf~
    #	emacs.d/themes/wombat-theme_ee.el
    #	scripts/backup.sh
    #

--- emacs.d/modules/my-navigation.el
+++ emacs.d/modules/my-navigation.el
@@ -194,6 +194,7 @@
     (bind-key "C-h a" #'counsel-apropos)
     (bind-key "C-x r b" #'counsel-bookmark)
     (bind-key "C-x b" #'counsel-ibuffer)
+    (bind-key "C-x C-b" #'pop-to-buffer)
 
     ;; (global-set-key (kbd "C-c g") 'counsel-git)
     ;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
