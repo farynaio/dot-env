05326591e0b830ee5bd1de1228203423445ffb7d
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2019-08-10 01:00:19 +0100

    emacs: set prefered encoding

--- emacs.d/modules/my-edit.el
+++ emacs.d/modules/my-edit.el
@@ -126,7 +126,11 @@ $0`(yas-escape-text yas-selected-text)`")
 ;; 				(progn
 ;; 					(setq wgrep-enable-key "r")))))
 
-(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment 'utf-8)
 
 (setq visual-line-fringe-indicators '(left-curly-arrow nil))
 
