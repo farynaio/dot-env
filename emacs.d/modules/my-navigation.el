2ca62daaf2a0d87fdb2417d6581dfa318e5c6486
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2019-08-16 10:37:08 +0100

    emacs: toggle horizontal/vertical windows split

--- emacs.d/modules/my-navigation.el
+++ emacs.d/modules/my-navigation.el
@@ -415,12 +415,39 @@ point reaches the beginning or end of the buffer, stop there."
   (clone-indirect-buffer nil t)
   (windmove-left))
 
+; https://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun my/toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
 ;; org mode conflicts resolution: windmove
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
 
+(global-set-key (kbd "C-x |") #'my/toggle-window-split)
 (global-set-key [remap move-beginning-of-line] 'my/smarter-move-beginning-of-line)
 
 (provide 'my-navigation)
