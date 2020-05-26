e69024a37aaf2a87e911ad4b29fca4f73c091d42
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-05-26 10:26:26 +0100

    emacs: org babel better code formatting

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -387,6 +387,7 @@ See also: https://stackoverflow.com/questions/9547912/emacs-calendar-show-more-t
 ;; (setq org-track-ordered-property-with-tag t)
 (setq org-use-property-inheritance t)
 (setq org-use-speed-commands t)
+(setq org-edit-src-content-indentation 0)
 (setq org-src-fontify-natively t)
 (setq org-src-tab-acts-natively t)
 (setq org-priority-start-cycle-with-default nil)
