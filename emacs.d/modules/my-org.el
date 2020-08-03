bd269a71bc1490bd4921d52d4707bbba46fb2b3e
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-08-03 20:19:03 +0100

    emacs: org-capture journal entry

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -622,6 +622,9 @@ SCHEDULED: %(org-insert-time-stamp (time-add (current-time) (days-to-time 372)) 
 - what I did today
   -
 
+- changes to make
+  -
+
 "
 :prepend t :jump-to-captured t :empty-lines-after 1 :kill-buffer t)
 
