5f05784438bfef23163794bc6b893e3fe2d2e6b8
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-08-04 22:25:52 +0100

    emacs: org-agenda fixed tasks order

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -1019,7 +1019,7 @@ Blood type/flavour: %^{Blood type: }
                 (org-agenda-skip-entry-if 'todo '("WAITING"))
                 (my/org-skip-subtree-if-priority ?A)
                 ))
-           (org-agenda-sorting-strategy '(todo-state-down time-up priority-down effort-down habit-down alpha-up))
+           (org-agenda-sorting-strategy '(time-up todo-state-down priority-down effort-down habit-down alpha-up))
             (org-agenda-remove-tags nil)
             (ps-number-of-columns 2)
             (ps-landscape-mode 1)
