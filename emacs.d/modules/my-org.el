d38b36457e8474f91284f6ab34e552cc52eb6d46
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-09-03 23:11:43 +0100

    emacs: org-agenda closed tasks reported in the day when they were closed

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -165,6 +165,7 @@ See also: https://stackoverflow.com/questions/9547912/emacs-calendar-show-more-t
      (setq org-startup-with-inline-images nil)
      (setq org-blank-before-new-entry '((heading . auto) (plain-list-item . nil)))
      (setq org-hide-emphasis-markers t)
+     (setq org-agenda-start-with-log-mode t)
 
      (bind-key "C-c l"         #'org-store-link                      org-mode-map)
      (bind-key "C-."           #'imenu-anywhere                      org-mode-map)
