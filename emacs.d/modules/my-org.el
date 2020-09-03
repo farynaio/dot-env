8de8bf7eb95ea0c640ffaa564726e7565befa366
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-09-03 18:06:47 +0100

    emacs: ivy-rich fixed org-roam title display

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -1599,7 +1599,9 @@ it can be passed in POS."
       (if (ivy-rich-switch-buffer-user-buffer-p candidate)
         (let ((file (buffer-file-name (get-buffer candidate))))
           (if (org-roam--org-roam-file-p file)
-            (org-roam--get-title-or-slug file)
+            (condition-case nil
+              (org-roam--get-title-or-slug file)
+              (error ""))
             ""))
         ""))
 
@@ -1608,13 +1610,48 @@ it can be passed in POS."
          (:columns
            ((jarfar/ivy-rich-switch-buffer-icon (:width 2))
              (ivy-rich-candidate (:width 30))
-             ;; (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
              (jarfar/ivy-switch-buffer-org-roam-title (:width 40))
-             ;; (ivy-rich-switch-buffer-size (:width 7))
-             ;; (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
-             ;; (ivy-rich-switch-buffer-project (:width 15 :face success))
              (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
-           :predicate (lambda (cand) (get-buffer cand)))))
+           :predicate (lambda (cand) (get-buffer cand)))
+         counsel-find-file
+         (:columns
+           ((ivy-read-file-transformer)
+             (ivy-rich-counsel-find-file-truename
+               (:face font-lock-doc-face))))
+         counsel-M-x
+         (:columns
+           ((counsel-M-x-transformer
+              (:width 40))
+             (ivy-rich-counsel-function-docstring
+               (:face font-lock-doc-face))))
+         counsel-describe-function
+         (:columns
+           ((counsel-describe-function-transformer
+              (:width 40))
+             (ivy-rich-counsel-function-docstring
+               (:face font-lock-doc-face))))
+         counsel-describe-variable
+         (:columns
+           ((counsel-describe-variable-transformer
+              (:width 40))
+             (ivy-rich-counsel-variable-docstring
+               (:face font-lock-doc-face))))
+         counsel-recentf
+         (:columns
+           ((ivy-rich-candidate
+              (:width 0.8))
+             (ivy-rich-file-last-modified-time
+               (:face font-lock-comment-face))))
+         package-install
+         (:columns
+           ((ivy-rich-candidate
+              (:width 30))
+             (ivy-rich-package-version
+               (:width 16 :face font-lock-comment-face))
+             (ivy-rich-package-archive-summary
+               (:width 7 :face font-lock-builtin-face))
+             (ivy-rich-package-install-summary
+               (:face font-lock-doc-face))))))
 
     (ivy-rich-mode 1)
     ))
