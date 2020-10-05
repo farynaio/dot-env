2e76ff6862ba632aa4d2176441753d9eb7645eee
Adam Faryna <11430365+adamfaryna@users.noreply.github.com>
2020-10-05 10:56:54 +0200

    emacs: fixed ledger exec path

--- dotenv/shell.sh
+++ dotenv/shell.sh
@@ -130,7 +130,7 @@ export MU_HOME="~/Maildir"
 export NPM_PACKAGES="$HOME/.npm-packages"
 export NODE_PATH="$NPM_PACKAGES/bin"
 export NODE_PATH=$NODE_PATH:$NPM_PACKAGES/lib/node_modules
-export LEDGER_FILE="$HOME/Dropbox/emacs/ledger/private.ledger.gpg"
+export LEDGER_FILE="$HOME/Documents/emacs/ledger/private.ledger.gpg"
 
 if [[ -e $HOME/bin ]]; then
   export PATH="$HOME/bin:$PATH"
