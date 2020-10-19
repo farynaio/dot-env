d69bd985b01e3b34f9d87f3cceba94b8d1e9985d
sanczol <adamfaryna@gmail.com>
2020-10-19 23:21:50 +0100

    emacs: theme bit of improvements

--- emacs.d/modules/my-theme.el
+++ emacs.d/modules/my-theme.el
@@ -18,8 +18,9 @@
 
   (custom-theme-set-faces
     'wombat
-    '(cursor ((t (:inherit nil :underline nil :background "White"))))
-    '(region ((t (:inherit nil :underline nil :foreground "White" :background "RoyalBlue4"))))
+    '(cursor ((t (:inherit nil :underline nil :background "gray82"))))
+    '(region ((t (:background "DodgerBlue4" :foreground "#f6f3e8"))))
+    ;; '(region ((t (:inherit nil :underline nil :foreground "White" :background "RoyalBlue4"))))
     '(hl-line ((t (:inherit nil :background "gray34"))))
     '(ledger-font-xact-highlight-face ((t nil)))
     '(org-level-2 ((t (:inherit outline-2 :foreground "LightGoldenrod2"))))
