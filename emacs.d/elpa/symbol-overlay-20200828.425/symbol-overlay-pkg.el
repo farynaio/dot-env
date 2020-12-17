7296b2d043b5811eb6aabde957ad039259eb2fac
sanczol <adamfaryna@gmail.com>
2020-12-17 16:31:10 +0000

    emacs: symbol-overlay package

---
+++ emacs.d/elpa/symbol-overlay-20200828.425/symbol-overlay-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from symbol-overlay.el  -*- no-byte-compile: t -*-
+(define-package "symbol-overlay" "20200828.425" "Highlight symbols with keymap-enabled overlays" '((emacs "24.3") (seq "2.2")) :commit "39f772b531117edba596e7a1210b3dbb87d56adb" :keywords '("faces" "matching") :authors '(("wolray" . "wolray@foxmail.com")) :maintainer '("wolray" . "wolray@foxmail.com") :url "https://github.com/wolray/symbol-overlay/")
