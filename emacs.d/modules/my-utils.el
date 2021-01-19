0cabc570824c56c8d1d48c8ad8a354ada21ea1dc
farynaio <adamfaryna@gmail.com>
2021-01-19 13:06:15 +0000

    emacs: added check if online/offline

--- emacs.d/modules/my-utils.el
+++ emacs.d/modules/my-utils.el
@@ -5,4 +5,9 @@
       (apply (car i) (cdr i))
       (funcall i))))
 
+;; https://emacs.stackexchange.com/a/18515/18445
+(defun my/online-p (&optional host)
+    (= 0 (call-process "ping" nil nil nil "-c" "1" "-W" "1"
+                       (if host host "www.google.com"))))
+
 (provide 'my-utils)
