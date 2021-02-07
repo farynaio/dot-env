8734af2d1256e8562dc5a0f096aed75b9e389cb7
farynaio <adamfaryna@gmail.com>
2021-02-07 17:48:34 +0000

    emacs: w3m images loaded by default

--- emacs.d/modules/my-www.el
+++ emacs.d/modules/my-www.el
@@ -46,7 +46,7 @@
     w3m-search-default-engine "duckduckgo"
     w3m-terminal-coding-system 'utf-8
     w3m-display-mode 'tabbed
-    w3m-default-display-inline-image nil
+    w3m-default-display-inline-image t
     w3m-confirm-leaving-secure-page nil
     w3m-new-session-in-background t
     w3m-new-session-url "about:"
