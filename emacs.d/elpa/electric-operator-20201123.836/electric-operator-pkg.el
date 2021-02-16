e68fa73d368bb2dcc405b09b26c40c81ad068cfa
farynaio <adamfaryna@gmail.com>
2021-02-16 00:35:18 +0000

    emacs: new packages

---
+++ emacs.d/elpa/electric-operator-20201123.836/electric-operator-pkg.el
@@ -0,0 +1,13 @@
+(define-package "electric-operator" "20201123.836" "Automatically add spaces around operators"
+  '((dash "2.10.0")
+    (emacs "24.4"))
+  :commit "9856ff8e1351086d0d738c1db300af63d6c085ba" :authors
+  '(("David Shepherd" . "davidshepherd7@gmail.com"))
+  :maintainer
+  '("David Shepherd" . "davidshepherd7@gmail.com")
+  :keywords
+  '("electric")
+  :url "https://github.com/davidshepherd7/electric-operator")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/major-mode-hydra-20191030.2354/major-mode-hydra-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from major-mode-hydra.el  -*- no-byte-compile: t -*-
+(define-package "major-mode-hydra" "20191030.2354" "Major mode keybindings managed by Hydra" '((dash "2.15.0") (pretty-hydra "0.2.2") (emacs "25")) :commit "20362323f66883c1336ffe70be24f91509addf54" :authors '(("Jerry Peng" . "pr2jerry@gmail.com")) :maintainer '("Jerry Peng" . "pr2jerry@gmail.com") :url "https://github.com/jerrypnz/major-mode-hydra.el")
