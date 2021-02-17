aee76724c3b6f2c40d19df0dd0166b409fcc8ed7
farynaio <adamfaryna@gmail.com>
2021-02-17 21:34:46 +0000

    emacs: new package and update

---
+++ emacs.d/elpa/dashboard-20210217.1535/dashboard-pkg.el
@@ -0,0 +1,13 @@
+(define-package "dashboard" "20210217.1535" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3")
+    (page-break-lines "0.11"))
+  :commit "fd7b6546e01fc5c64e8496ca0e1f8b20db779fa4" :authors
+  '(("Rakan Al-Hneiti"))
+  :maintainer
+  '("Rakan Al-Hneiti")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
