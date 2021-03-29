86d00b0e73019b132507e418040d8fac57dda01a
farynaio <adamfaryna@gmail.com>
2021-03-29 10:54:24 +0100

    emacs: dired clean modeline

--- emacs.d/modules/my-theme.el
+++ emacs.d/modules/my-theme.el
@@ -33,6 +33,7 @@
         (setq vc-mode noback)))))
 
 (use-package all-the-icons-dired
+  :diminish all-the-icons-dired-mode
   :after all-the-icons
   :config
   (add-hook 'dired-mode-hook #'all-the-icons-dired-mode))
