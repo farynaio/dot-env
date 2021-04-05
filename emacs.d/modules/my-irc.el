ca8207266e8cf1bef21823472517a404e862bb93
farynaio <adamfaryna@gmail.com>
2021-04-05 20:10:59 +0100

    emacs: irc fixed config

--- emacs.d/modules/my-irc.el
+++ emacs.d/modules/my-irc.el
@@ -7,7 +7,7 @@
           ("C-w q" . evil-quit)
           ("C-w v" . evil-window-vsplit)
           ("C-c C-j" . my/erc-join-channel))
-  :hook ((erc-send-pre . my/erc-preprocess))
+  ;; :hook ((erc-send-pre . my/erc-preprocess))
   :custom-face
   (erc-action-face ((t (:foreground "#8fbcbb"))))
   (erc-error-face ((t (:foreground "#bf616a"))))
@@ -28,7 +28,8 @@
   (erc-log-mode t)
   (erc-log-write-after-insert t)
   (erc-log-write-after-send t)
-  ;; (erc-prompt-for-password nil)
+  (erc-save-buffer-on-part nil)
+  (erc-save-queries-on-quit nil)
   (erc-query-display 'window)
   (erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
                               "324" "329" "332" "333" "353" "477"))
@@ -38,17 +39,19 @@
   (erc-user-mode "iR")
   (erc-header-line-format "%n on %t (%m) %o")
   (erc-join-buffer 'bury)
-  ;; (erc-kill-buffer-on-part t)
   (erc-lurker-threshold-time 43200)
   (erc-server-reconnect-timeout 3)
   (erc-server-reconnect-attempts 5)
   (erc-reuse-buffers nil)
   (erc-dcc-get-default-directory "~/Download")
   (erc-log-channels-directory  my/erc-logs)
+  ;; (erc-prompt-for-password nil)
+  ;; (erc-kill-buffer-on-part t)
   :config
   (require 'erc-services)
   ;; (require 'erc-join)
 
+  (setq erc-debug-irc-protocol t)
   (setq erc-modules (nconc erc-modules '(autoaway dcc irccontrols keep-place move-to-prompt spelling noncommands readonly services stamp track)))
 
   (erc-autojoin-mode -1)
@@ -77,8 +80,6 @@
   ;;       (erc-autojoin-channels erc-session-server (erc-current-nick))
   ;; nil)))
 
-  (setq my/erc-channel-list '("#bitcoin" "#bitcoin-pricetalk" "#erc" "#znc" "##privacy" "##linux"))
-
   (defun my/erc-join-channel (channel &optional key)
     "Join CHANNEL. If `point' is at the beginning of a channel name, use that as default."
     (interactive
@@ -87,7 +88,7 @@
                (table (when (erc-server-buffer-live-p)
                         (set-buffer (process-buffer erc-server-process))
                         erc-channel-list)))
-          (completing-read (format-prompt "Join channel" chnl)
+          (completing-read "Join channel"
             my/erc-channel-list nil nil nil nil chnl))
         (when (or current-prefix-arg erc-prompt-for-channel-key)
           (read-from-minibuffer "Channel key (RET for none): " nil))))
@@ -96,50 +97,55 @@
   (add-hook 'erc-join-hook (lambda () (switch-to-buffer (current-buffer))))
 
   ;; https://www.reddit.com/r/emacs/comments/8ml6na/tip_how_to_make_erc_fun_to_use/
-  (defun my/erc-start-or-switch ()
-    "Connects to ERC, or switch to last active buffer."
+  (defun my/erc-freenode ()
+    "Connects to Freenode, or switch to last active buffer."
     (interactive)
     (let ((tls-checktrust nil)
-           (gnutls-verify-error nil)
-           (freenodeBuffer (get-buffer "nil")))
-      (if freenodeBuffer
-        (display-buffer freenodeBuffer)
-        (erc-tls :server my/znc-server :port my/znc-server-port :nick my/znc-server-nick :password my/znc-network-password-freenode)
+           (gnutls-verify-error nil))
+      (erc-tls :server my/znc-server :port my/znc-server-port :nick my/znc-server-nick-freenode :password my/znc-network-password-freenode)))
+
+  (defun my/erc-oftc ()
+    "Connects to OFTC, or switch to last active buffer."
+    (interactive)
+    (let ((tls-checktrust nil)
+           (gnutls-verify-error nil))
+      (erc-tls :server my/znc-server :port my/znc-server-port :nick my/znc-server-nick-oftc :password my/znc-network-password-oftc)))
+
+        ;; (erc-tls :server "irc.oftc.net" :port "6697" :nick "apzeys" :password "9mHi2ekHAr3w8r2mKL3z")
         ;; (let ((user-login-name "azur12"))
         ;;   (erc-tls :server "chat.freenode.net" :port 6697 :nick my/erc-nick :password my/erc-freenode-password)
         ;;   )
-        )))
 
   ;; https://www.reddit.com/r/emacs/comments/8ml6na/tip_how_to_make_erc_fun_to_use/
-  (defun my/erc-count-users ()
-    "Displays the number of users connected on the current channel."
-    (interactive)
-    (if (get-buffer "freenode")
-      (let ((channel (erc-default-target)))
-        (if (and channel (erc-channel-p channel))
-          (message "%d users are online on %s"
-            (hash-table-count erc-channel-users)
-            channel)
-          (user-error "The current buffer is not a channel")))
-      (user-error "You must first start ERC")))
+  ;; (defun my/erc-count-users ()
+  ;;   "Displays the number of users connected on the current channel."
+  ;;   (interactive)
+  ;;   (if (get-buffer "freenode")
+  ;;     (let ((channel (erc-default-target)))
+  ;;       (if (and channel (erc-channel-p channel))
+  ;;         (message "%d users are online on %s"
+  ;;           (hash-table-count erc-channel-users)
+  ;;           channel)
+  ;;         (user-error "The current buffer is not a channel")))
+  ;;     (user-error "You must first start ERC")))
 
-  (defun my/erc-get-ops ()
-    "Displays the names of ops users on the current channel."
-    (interactive)
-    (if (get-buffer "freenode")
-        (let ((channel (erc-default-target)))
-          (if (and channel (erc-channel-p channel))
-              (let (ops)
-                (maphash (lambda (nick cdata)
-                           (if (and (cdr cdata)
-                                    (erc-channel-user-op (cdr cdata)))
-                               (setq ops (cons nick ops))))
-                         erc-channel-users)
-                (if ops
-                    (message "The online ops users are: %s"  (mapconcat 'identity ops " "))
-                  (message "There are no ops users online on %s" channel)))
-            (user-error "The current buffer is not a channel")))
-      (user-error "You must first be connected on IRC")))
+  ;; (defun my/erc-get-ops ()
+  ;;   "Displays the names of ops users on the current channel."
+  ;;   (interactive)
+  ;;   (if (get-buffer "freenode")
+  ;;       (let ((channel (erc-default-target)))
+  ;;         (if (and channel (erc-channel-p channel))
+  ;;             (let (ops)
+  ;;               (maphash (lambda (nick cdata)
+  ;;                          (if (and (cdr cdata)
+  ;;                                   (erc-channel-user-op (cdr cdata)))
+  ;;                              (setq ops (cons nick ops))))
+  ;;                        erc-channel-users)
+  ;;               (if ops
+  ;;                   (message "The online ops users are: %s"  (mapconcat 'identity ops " "))
+  ;;                 (message "There are no ops users online on %s" channel)))
+  ;;           (user-error "The current buffer is not a channel")))
+  ;;     (user-error "You must first be connected on IRC")))
 
   (defun my/erc-preprocess (string)
     "Avoids channel flooding."
@@ -174,33 +180,33 @@
   ;;     q))
   )
 
-(if (file-exists-p (concat user-emacs-directory "lisp/erc-sasl.el"))
-  (use-package erc-sasl
-    :load-path "lisp/"
-    :after erc
-    :config
-    (add-to-list 'erc-sasl-server-regexp-list "chat\\.freenode\\.net")
-    (defun erc-login ()
-      "Perform user authentication at the IRC server."
-      (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-                 (erc-current-nick)
-                 (user-login-name)
-                 (or erc-system-name (system-name))
-                 erc-session-server
-                 erc-session-user-full-name))
-      (if erc-session-password
-        (erc-server-send (format "PASS %s" erc-session-password))
-        (message "Logging in without password"))
-      (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
-        (erc-server-send "CAP REQ :sasl"))
-      (erc-server-send (format "NICK %s" (erc-current-nick)))
-      (erc-server-send
-        (format "USER %s %s %s :%s"
-          ;; hacked - S.B.
-          (if erc-anonymous-login erc-email-userid (user-login-name))
-          "0" "*"
-          erc-session-user-full-name))
-      (erc-update-mode-line))))
+;; (if (file-exists-p (concat user-emacs-directory "lisp/erc-sasl.el"))
+;;   (use-package erc-sasl
+;;     :load-path "lisp/"
+;;     :after erc
+;;     :config
+;;     (add-to-list 'erc-sasl-server-regexp-list "chat\\.freenode\\.net")
+;;     (defun erc-login ()
+;;       "Perform user authentication at the IRC server."
+;;       (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+;;                  (erc-current-nick)
+;;                  (user-login-name)
+;;                  (or erc-system-name (system-name))
+;;                  erc-session-server
+;;                  erc-session-user-full-name))
+;;       (if erc-session-password
+;;         (erc-server-send (format "PASS %s" erc-session-password))
+;;         (message "Logging in without password"))
+;;       (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
+;;         (erc-server-send "CAP REQ :sasl"))
+;;       (erc-server-send (format "NICK %s" (erc-current-nick)))
+;;       (erc-server-send
+;;         (format "USER %s %s %s :%s"
+;;           ;; hacked - S.B.
+;;           (if erc-anonymous-login erc-email-userid (user-login-name))
+;;           "0" "*"
+;;           erc-session-user-full-name))
+;;       (erc-update-mode-line))))
 
 (use-package erc-sasl
   :after erc
@@ -235,6 +241,8 @@
 (use-package erc-hl-nicks
   :after erc)
 
-(defalias 'irc #'my/erc-start-or-switch)
+(defalias 'irc #'my/erc-freenode)
+(defalias 'irc-freenode #'my/erc-freenode)
+(defalias 'irc-oftc #'my/erc-oftc)
 
 (provide 'my-irc)
