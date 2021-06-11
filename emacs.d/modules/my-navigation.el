e8ed55cf2f9369d7083d3be748754d7a3b775cac
farynaio <adamfaryna@gmail.com>
2021-06-11 01:40:10 +0100

    emacs: added pixel scroll mode config

--- emacs.d/modules/my-navigation.el
+++ emacs.d/modules/my-navigation.el
@@ -13,6 +13,21 @@
   scroll-conservatively 101
   auto-window-vscroll nil)
 
+;;; Scrolling.
+;; Good speed and allow scrolling through large images (pixel-scroll).
+;; Note: Scroll lags when point must be moved but increasing the number
+;;       of lines that point moves in pixel-scroll.el ruins large image
+;;       scrolling. So unfortunately I think we'll just have to live with
+;;       this.
+;; https://emacs.stackexchange.com/a/42158/18445
+(setq pixel-dead-time 0) ; Never go back to the old scrolling behaviour.
+(setq pixel-resolution-fine-flag t) ; Scroll by number of pixels instead of lines (t = frame-char-height pixels).
+(setq mouse-wheel-scroll-amount '(1)) ; Distance in pixel-resolution to scroll each mouse wheel event.
+(setq mouse-wheel-progressive-speed nil) ; Progressive speed is too fast for me.
+(setq fast-but-imprecise-scrolling t) ; No (less) lag while scrolling lots.
+(setq jit-lock-defer-time 0) ; Just don't even fontify if we're still catching up on user input.
+
+
 (setq display-buffer-alist '(("\\*[hH]elp.*"
                               (display-buffer-reuse-window display-buffer-at-bottom)
                                (window-width . 0.5)
