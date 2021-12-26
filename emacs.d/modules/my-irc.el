235ef9b8a1aaad31f00bcbaddaa599cae50da805
farynaio <adam22@lifeisgood.cyou>
2021-12-26 15:21:18 +0000

    emacs: erc-sasl conditional wrap

--- emacs.d/modules/my-irc.el
+++ emacs.d/modules/my-irc.el
@@ -216,32 +216,37 @@
 ;;           erc-session-user-full-name))
 ;;       (erc-update-mode-line))))
 
-(use-package erc-sasl
-  :after erc
-  :ensure nil
-  :config
-  (add-to-list 'erc-sasl-server-regexp-list my/znc-server-regex)
-  (defun erc-login ()
-    "Perform user authentication at the IRC server."
-    (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-               (erc-current-nick)
-               (user-login-name)
-               (or erc-system-name (system-name))
-               erc-session-server
-               erc-session-user-full-name))
-    (if erc-session-password
-      (erc-server-send (format "PASS %s" erc-session-password))
-      (message "Logging in without password"))
-    (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
-      (erc-server-send "CAP REQ :sasl"))
-    (erc-server-send (format "NICK %s" (erc-current-nick)))
-    (erc-server-send
-      (format "USER %s %s %s :%s"
-        ;; hacked - S.B.
-        (if erc-anonymous-login erc-email-userid (user-login-name))
-        "0" "*"
-        erc-session-user-full-name))
-    (erc-update-mode-line)))
+(if my/znc-server-regex
+  (use-package erc-sasl
+    :after erc
+    :ensure nil
+    :config
+    (add-to-list 'erc-sasl-server-regexp-list my/znc-server-regex)
+    (defun erc-login ()
+      "Perform user authentication at the IRC server."
+      (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+                 (erc-current-nick)
+                 (user-login-name)
+                 (or erc-system-name (system-name))
+                 erc-session-server
+                 erc-session-user-full-name))
+      (if erc-session-password
+        (erc-server-send (format "PASS %s" erc-session-password))
+        (message "Logging in without password"))
+      (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
+        (erc-server-send "CAP REQ :sasl"))
+      (erc-server-send (format "NICK %s" (erc-current-nick)))
+      (erc-server-send
+        (format "USER %s %s %s :%s"
+          ;; hacked - S.B.
+          (if erc-anonymous-login erc-email-userid (user-login-name))
+          "0" "*"
+          erc-session-user-full-name))
+      (erc-update-mode-line))
+    )
+
+  (user-error "erc-sasl: variable 'my/znc-server-regex' not set, package disabled!")
+  )
 
 (use-package erc-image
   :after erc)
