7b43ead4a6c4cfb2810d696438d261361dc248ad
farynaio <adam22@lifeisgood.cyou>
2022-01-01 22:21:41 +0000

    emacs: auth-source-save-bahavior

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -349,6 +349,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(custom-enabled-themes '(wombat))
  '(ledger-reports
