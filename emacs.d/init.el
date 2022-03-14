05b625118bc55dfe184a109b08ee18e33bb8e951
farynaio <adam22@lifeisgood.cyou>
2022-03-14 18:44:44 +0000

    emacs: fixed open of initial buffers

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -243,8 +243,9 @@
 
 (defun jarfar/open-buffers-on-startup ()
   (if (file-exists-p my/notes-file-path)
-      (find-file my/notes-file-path)
-      (find-file my/whonix-share-file-path)
+      (progn
+        (find-file my/notes-file-path)
+        (find-file my/whonix-share-file-path))
     (switch-to-buffer "*scratch*")))
 
 (when (eq system-type 'darwin)
