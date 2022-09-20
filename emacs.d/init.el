94fa1ea598a02492cf869dd790b9753cd9562a3f
adamWithF <adam22@lifeisgood.cyou>
2022-09-20 22:12:58 +0100

    emacs: clean up basic config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,19 +4,16 @@
 ;; (toggle-debug-on-quit)
 ;; (setq auth-source-debug nil)
 
-(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/libgccjit/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin19/10.2.0")
-
 (setq
- gc-cons-threshold (* 1024 1024 2)
- ;; gc-cons-threshold (* 1024 800)
- gc-cons-percentage 0.1
- read-process-output-max (* 1024 1024 512))
+  gc-cons-threshold (* 1024 1024 2)
+  ;; gc-cons-threshold (* 1024 800)
+  gc-cons-percentage 0.1
+  read-process-output-max (* 1024 1024 512))
 ;; (setq gc-cons-threshold 402653184)
 ;; (setq gc-cons-percentage 0.6)
 
-(defun farynaio/debug-on-error-toggle ()
-  (interactive)
-  (setq debug-on-error (not debug-on-error)))
+(setenv "SHELL" (executable-find "bash"))
+(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/libgccjit/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin19/10.2.0")
 
 (eval-when-compile
   (defvar oauth--token-data ())
@@ -36,7 +33,6 @@
 ;; (setenv "LC_NUMERIC" "en_US.UTF-8")
 ;; (setenv "LC_TIME" "en_US.UTF-8")
 ;; (setenv "GPG_AGENT_INFO" nil)
-(setenv "SHELL" (executable-find "bash"))
 
 (setq shell-file-name "/bin/sh")
 ;; (setenv "PATH" (concat "/usr/local/opt/rbenv/shims:/usr/local/opt/rbenv/bin:" (getenv "PATH")))
@@ -48,18 +44,18 @@
     ;; (setenv "PATH" (concat "~/Applications/Firefox.app/Contents/MacOS:" (getenv "PATH")))
     ;; (add-to-list 'exec-path "/Applications/Firefox.app/Contents/MacOS")
     (setq
-     browse-url-generic-program "/Applications/Firefox.app/Contents/MacOS/firefox")))
+      browse-url-generic-program "/Applications/Firefox.app/Contents/MacOS/firefox")))
 
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(add-to-list 'load-path "~/.emacs.d/modules/")
-(add-to-list 'load-path "~/.emacs.d/modules/devel")
 ;; (add-to-list 'exec-path "/usr/local/opt/rbenv/shims")
 ;; (add-to-list 'exec-path "/usr/local/opt/rbenv/bin")
-
 (add-to-list 'exec-path "~/.npm-packages/bin")
 (add-to-list 'exec-path "~/.rbenv/bin")
 (add-to-list 'exec-path "~/.rbenv/shims")
 (add-to-list 'exec-path "/usr/local/bin")
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/modules/")
+(add-to-list 'load-path "~/.emacs.d/modules/devel")
 
 (setq package-enable-at-startup nil)
 
@@ -69,9 +65,9 @@
        (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -83,10 +79,7 @@
   straight-disable-compile nil
   straight-vc-git-default-protocol 'https
   straight-check-for-modifications '(check-on-save find-when-checking)
-)
-
-(require 'tls)
-(require 'gnutls)
+  )
 
 ;; https://www.reddit.com/r/emacs/comments/8sykl1/emacs_tls_defaults_are_downright_dangerous/
 ;;(setq
@@ -110,19 +103,17 @@
   (auto-compile-on-load-mode))
 
 (when (and (fboundp 'native-comp-available-p)
-           (native-comp-available-p))
+        (native-comp-available-p))
   (setq
-   comp-speed 2
-   comp-deferred-compilation t
-   package-native-compile t
-   comp-async-report-warnings-errors nil)
+    comp-speed 2
+    comp-deferred-compilation t
+    package-native-compile t
+    comp-async-report-warnings-errors nil)
   (add-to-list 'exec-path (concat invocation-directory "bin") t)
   (native--compile-async '("~/.emacs.d/lisp/" "~/.emacs.d/themes/" "~/.emacs.d/modules/" "~/.emacs.d/local-config.el" "~/.emacs.d/init.el") t))
 
-
 (use-package org)
 (use-package org-contrib)
-
 
 (setq load-prefer-newer t)
 
@@ -163,6 +154,19 @@
 
 (run-with-idle-timer (* 60 5) t 'garbage-collect)
 
+(when (eq system-type 'darwin)
+  (setq
+    mac-command-modifier 'super
+    ns-right-alternate-modifier nil))
+
+(when (fboundp 'set-charset-priority)
+  (set-charset-priority 'unicode))
+
+(set-default-coding-systems 'utf-8)
+
+(require 'tls)
+(require 'gnutls)
+
 ;; My modules
 (require 'my-path)
 (when (file-exists-p my/local-config-file-path)
@@ -194,29 +198,29 @@
 (defvar my-debug-activate nil)
 
 (setq
- my-utils-activate t
- my-edit-activate t
- my-git-activate t
- my-evil-activate t
- my-navigation-activate t
- my-org-activate t
- my-writing-activate t
- my-devel-activate t
- my-email-activate t
- my-dired-activate t
- my-notifications-activate nil
- my-www-activate t
- my-irc-activate t
- my-theme-activate t
- my-shell-activate t
- my-encrypt-activate t
- my-cleanup-activate t
- my-rss-activate t
- my-org-caldav-activate nil
- my-taskjuggler-activate nil
- my-hydra-activate t
- my-presentation-activate nil
- my-debug-activate t)
+  my-utils-activate t
+  my-edit-activate t
+  my-git-activate t
+  my-evil-activate t
+  my-navigation-activate t
+  my-org-activate t
+  my-writing-activate t
+  my-devel-activate t
+  my-email-activate t
+  my-dired-activate t
+  my-notifications-activate nil
+  my-www-activate t
+  my-irc-activate t
+  my-theme-activate t
+  my-shell-activate t
+  my-encrypt-activate t
+  my-cleanup-activate t
+  my-rss-activate t
+  my-org-caldav-activate nil
+  my-taskjuggler-activate nil
+  my-hydra-activate t
+  my-presentation-activate nil
+  my-debug-activate t)
 
 (when my-debug-activate (require 'my-debug))
 (when my-utils-activate (require 'my-utils))
@@ -242,48 +246,38 @@
 (when my-taskjuggler-activate (require 'my-taskjuggler))
 (when my-org-caldav-activate (require 'my-org-caldav))
 
-(when (eq system-type 'darwin)
-  (setq
-   mac-command-modifier 'super
-   ns-right-alternate-modifier nil))
-
-(when (fboundp 'set-charset-priority)
-  (set-charset-priority 'unicode))
-
-(set-default-coding-systems 'utf-8)
+(setq-default
+  ring-bell-function 'ignore
+  visible-bell nil
+  echo-keystrokes 0
+  create-lockfiles nil ; this should be safe as long I'm the only user of FS
+  enable-recursive-minibuffers nil ;; ?
+  apropos-do-all t
+  compilation-always-kill t
+  compilation-ask-about-save nil
+  compilation-scroll-output t
+  confirm-nonexistent-file-or-buffer nil
+  idle-update-delay 2
+  minibuffer-prompt-properties '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
+  initial-scratch-message nil
+  inhibit-startup-screen t
+  x-underline-at-descent-line t
+  process-connection-type nil
+  fill-column 99
+  default-directory "~/"
+  initial-buffer-choice t
+  confirm-kill-processes nil
+  password-cache-expiry nil)
 
 (setq-default
- ring-bell-function 'ignore
- visible-bell nil
- echo-keystrokes 0
- create-lockfiles nil ; this should be safe as long I'm the only user of FS
- enable-recursive-minibuffers nil ;; ?
- apropos-do-all t
- compilation-always-kill t
- compilation-ask-about-save nil
- compilation-scroll-output t
- confirm-nonexistent-file-or-buffer nil
- idle-update-delay 2
- minibuffer-prompt-properties '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
- initial-scratch-message nil
- inhibit-startup-screen t
- x-underline-at-descent-line t
- process-connection-type nil
- fill-column 99
- default-directory "~/"
- initial-buffer-choice t
- confirm-kill-processes nil
- password-cache-expiry nil)
-
-(setq-default
- make-backup-files nil
- backup-by-copying t
- kept-new-versions 5
- kept-old-versions 5
- delete-old-versions t
- version-control t
- auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))
- backup-directory-alist '(("." . "~/.emacs.d/backups")))
+  make-backup-files nil
+  backup-by-copying t
+  kept-new-versions 5
+  kept-old-versions 5
+  delete-old-versions t
+  version-control t
+  auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))
+  backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 (defun reload-config ()
 	"Reload config."
@@ -305,22 +299,22 @@
 (add-hook 'emacs-startup-hook 'my/protected-buffers 90)
 
 (setq safe-local-variable-values
-      '(
-        (org-hide-emphasis-markers . t)
-        (ispell-dictionary . "en")
-        (ispell-dictionary . "pl")
-        (my/language-local . "pl")
-        (my/language-local . "en")
-        (org-use-property-inheritance . t)
-        (org-confirm-babel-evaluate)
-        (eval progn
-              (add-hook
-               (quote js-mode-hook)
-               (lambda nil
-                 (flycheck-mode -1))
-               t ))
-        (browse-url-browser-function . my/browse-url-tor)
-        ))
+  '(
+     (org-hide-emphasis-markers . t)
+     (ispell-dictionary . "en")
+     (ispell-dictionary . "pl")
+     (my/language-local . "pl")
+     (my/language-local . "en")
+     (org-use-property-inheritance . t)
+     (org-confirm-babel-evaluate)
+     (eval progn
+       (add-hook
+         (quote js-mode-hook)
+         (lambda nil
+           (flycheck-mode -1))
+         t ))
+     (browse-url-browser-function . my/browse-url-tor)
+     ))
 
 ;; TODO is this working?
 ;; Disable warning on risky variables set on file local level.
@@ -346,55 +340,7 @@
 (find-file "~/.emacs.d/init.el")
 (find-file "~/.emacs.d/modules/my-hydra.el")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(company-show-quick-access t nil nil "Customized with use-package company")
- '(lsp-semantic-tokens-enable nil nil nil "Customized with use-package lsp-mode")
- '(org-agenda-files
-   '("~/Documents/roam/journal/2022-03-11.org" "~/Documents/emacs/orgs/Tasks/tasks.org.gpg"))
- '(package-selected-packages
-   '(expand-region use-package-ensure-system-package flycheck-ledger helpful visual-fill-column visual-fill-column-mode nov dired-narrow dired-subtree dired dashboard all-the-icons-dired counsel-projectile wiki-summary org-pomodoro hungry-delete electric-operator major-mode-hydra xml-mode iscroll mu4e-maildirs-extension openwith w3m evil-multiedit lsp-ui yaml-mode which-key web-mode web-beautify vue-mode vimrc-mode use-package undo-fu typescript-mode terraform-mode symbol-overlay smartscan request rainbow-mode rainbow-delimiters projectile-rails prettier-js prettier persistent-scratch org-roam-server org-review org-plus-contrib org-mime org-journal org-drill ob-async oauth2 magit ledger-mode langtool json-mode japanese-holidays jade-mode ivy-rich ivy-hydra hl-todo graphql-mode goto-last-change google-translate git-gutter geben flycheck evil-visualstar evil-surround evil-matchit evil-anzu eslintd-fix emojify emmet-mode elpy elfeed-web elfeed-org elfeed-goodies editorconfig dtrt-indent drag-stuff dockerfile-mode diminish dap-mode counsel company-php company-org-roam calfw-org calfw auto-highlight-symbol auto-compile artbollocks-mode all-the-icons ag add-node-modules-path))
- '(safe-local-variable-values
-   '((j2-basic-offset . 4)
-     (j2-basic-offset . 2)
-     (css-indent-offset . 4)
-     (graphql-indent-level . 4)
-     (sgml-basic-offset . 4)
-     (nil . dtrt-indent-mode)
-     (org-hide-emphasis-markers . t)
-     (ispell-dictionary . "en")
-     (ispell-dictionary . "pl")
-     (my/language-local . "pl")
-     (my/language-local . "en")
-     (org-use-property-inheritance . t)
-     (org-confirm-babel-evaluate)
-     (eval progn
-           (add-hook 'js-mode-hook
-                     (lambda nil
-                       (flycheck-mode -1))
-                     t)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:inherit nil :background "gray82" :underline nil))))
- '(erc-action-face ((t (:foreground "#8fbcbb"))))
- '(erc-error-face ((t (:foreground "#bf616a"))))
- '(erc-input-face ((t (:foreground "#ebcb8b"))))
- '(erc-notice-face ((t (:foreground "#ebcb8b"))))
- '(erc-timestamp-face ((t (:foreground "#a3be8c"))))
- '(hl-line ((t (:background "gray32"))))
- '(org-agenda-dimmed-todo-face ((t (:foreground "gray50"))))
- '(org-level-2 ((t (:inherit default :extend nil :foreground "goldenrod"))))
- '(org-level-3 ((t (:inherit nil :extend nil :foreground "coral"))))
- '(org-level-4 ((t (:inherit nil :extend nil :foreground "burlywood1"))))
- '(org-priority ((t (:inherit font-lock-keyword-face :foreground "gold3"))))
- '(persp-selected-face ((t (:foreground "cyan4" :weight bold))))
- '(region ((t (:background "DodgerBlue4" :foreground "#f6f3e8"))))
- '(symbol-overlay-default-face ((t (:background "gray37")))))
+(unless (file-exists-p (expand-file-name "custom.el" user-emacs-directory))
+  (error (concat "custom.el file not exists. Create one from custo.el.sample")))
+
+(setq custom-file "~/.emacs.d/custom.el")
