f05bfbe023bebcee4501faef93eb4fd99e6b2082
adamWithF <adam22@lifeisgood.cyou>
2022-09-20 22:56:31 +0100

    emacs: hunspell by default

--- emacs.d/modules/my-writing.el
+++ emacs.d/modules/my-writing.el
@@ -52,8 +52,8 @@
   (ispell-dictionary-alist
     '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
        ("fr_FR" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "fr_FR") nil utf-8)))
-  ;; (ispell-program-name (executable-find "hunspell"))
-  (ispell-program-name (executable-find "aspell"))
+  (ispell-program-name (executable-find "hunspell"))
+  ;; (ispell-program-name (executable-find "aspell"))
   (ispell-really-hunspell t)
   (ispell-silently-savep t)
   (ispell-extra-args '("--sug-mode=ultra"))
