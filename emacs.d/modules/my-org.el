c2f70dc32c5d3463db997f6dcd055e31e73b4648
adamWithF <adam10@puremail.cyou>
2022-11-19 12:45:59 +0000

    emacs: added org-mind-map package

--- emacs.d/modules/my-org.el
+++ emacs.d/modules/my-org.el
@@ -1337,4 +1337,22 @@ it can be passed in POS."
 ;; Fixes problem with void function org-clocking-buffer
 (defun org-clocking-buffer ())
 
+;; This is an Emacs package that creates graphviz directed graphs from
+;; the headings of an org file
+(use-package org-mind-map
+  :init
+  (require 'ox-org)
+  ;; :ensure t
+  ;; Uncomment the below if 'ensure-system-packages` is installed
+  ;;:ensure-system-package (gvgen . graphviz)
+  :config
+  (setq org-mind-map-engine "dot")       ; Default. Directed Graph
+  ;; (setq org-mind-map-engine "neato")  ; Undirected Spring Graph
+  ;; (setq org-mind-map-engine "twopi")  ; Radial Layout
+  ;; (setq org-mind-map-engine "fdp")    ; Undirected Spring Force-Directed
+  ;; (setq org-mind-map-engine "sfdp")   ; Multiscale version of fdp for the layout of large graphs
+  ;; (setq org-mind-map-engine "twopi")  ; Radial layouts
+  ;; (setq org-mind-map-engine "circo")  ; Circular Layout
+  )
+
 (provide 'my-org)
