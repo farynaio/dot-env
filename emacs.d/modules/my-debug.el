946d5705e8446650f0d1025b938d6dbd849668c0
adamWithF <adam10@puremail.cyou>
2023-08-06 17:52:52 +0100

    emacs: added find no ascii debug function

--- emacs.d/modules/my-debug.el
+++ emacs.d/modules/my-debug.el
@@ -1,3 +1,8 @@
 (add-hook 'profiler-report-mode-hook (lambda () (hl-line-mode 1)))
 
+(defun my/occur-non-ascii ()
+  "Find any non-ascii characters in the current buffer."
+  (interactive)
+  (occur "[^[:ascii:]]"))
+
 (provide 'my-debug)
