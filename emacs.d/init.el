7e586f64432eae35f0d5c0ad21c34b5126770162
farynaio <adam10@puremail.cyou>
2023-08-27 20:23:13 +0100

    Emacs: clean up Emacs-local-config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,6 +11,8 @@
 (setenv "SHELL" (executable-find "bash"))
 (setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/libgccjit/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin19/10.2.0")
 
+(setq-default auth-sources '("~/.authinfo.gpg" "~/.netrc.gpg" "~/.authinfo" "~/.netrc"))
+
 (eval-when-compile
   (defvar oauth--token-data ())
   (defvar url-http-method ())
