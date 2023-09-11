b1992fff556199ab8f6d6604e28b174b99f71f2f
farynaio <adam10@puremail.cyou>
2023-09-11 15:52:52 +0100

    Emacs: clean Ruby config

--- emacs.d/modules/devel/my-ruby.el
+++ emacs.d/modules/devel/my-ruby.el
@@ -1,3 +1,6 @@
+
+;;; Code:
+
 ;; (use-package robe
 ;; :config
 ;; (progn
@@ -13,4 +16,20 @@
   :requires projectile
   :hook (ruby-mode . (lambda () (when (projectile-mode) (projectile-rails-on)))))
 
+;; (use-package flymake-ruby)
+
+;; https://melpa.org/#/robe
+;; https://github.com/dmexe/emacs-rails-reloaded ; unmaintaned
+;; https://www.emacswiki.org/emacs/ri.el ; completion lookup tool
+;; https://melpa.org/#/ruby-tools
+;; https://melpa.org/#/ruby-refactor
+;; https://melpa.org/#/ruby-interpolation
+;; https://melpa.org/#/ruby-extra-highlight
+;; https://melpa.org/#/ruby-electric
+;; https://melpa.org/#/ruby-end
+;; https://melpa.org/#/rspec-mode
+;; https://melpa.org/#/rbtagger :: code navigation
+;; https://melpa.org/#/format-all
+
 (provide 'my-ruby)
+;;; my-ruby.el ends here
