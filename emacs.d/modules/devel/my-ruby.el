f3e71724f08602513633704c65972b6f9df411ba
adamWithF <adam10@puremail.cyou>
2023-09-15 12:01:10 +0100

    Emacs: Ruby major-mode-hydra init

--- emacs.d/modules/devel/my-ruby.el
+++ emacs.d/modules/devel/my-ruby.el
@@ -1,33 +1,34 @@
 
 ;;; Code:
 
-;; (use-package robe
-;; :config
-;; (progn
-;;   (add-hook 'robe-mode-hook (lambda ()
-;; (robe-start)
-;;                               (make-local-variable 'company-backends)
-;;                               (add-to-list 'company-backends 'company-robe t)))
-;;   (add-hook 'ruby-mode-hook 'robe-mode)
-;;   ))
-
 (use-package inf-ruby
   :commands inf-ruby-minor-mode
   :config
+  (add-hook 'compilation-filter-hook #'inf-ruby-auto-enter)
   (add-to-list 'evil-emacs-state-modes 'inf-ruby-mode)
-  ;; (evil-set-initial-state 'inf-ruby-minor-mode 'emacs)
-
-  ;; (add-hook 'compilation-filter-hook #'inf-ruby-auto-enter)
-
   ;; (add-hook 'compilation-filter-hook #'inf-ruby-auto-enter-and-focus)
   )
 
 (use-package ruby-mode
   :straight nil
   :mode "\\.rb\\'"
-  :hook ((ruby-mode . seeing-is-believing)
-          (ruby-mode . inf-ruby-minor-mode)
-          ))
+  :hook ((ruby-mode . inf-ruby-minor-mode)
+          (ruby-mode . eglot-ensure))
+  :config
+  (major-mode-hydra-define+ ruby-mode
+    (:hint nil :color amaranth :quit-key "q" :title (with-alltheicon "ruby" "Ruby" 1 -0.05))
+    ("Assess"
+      (("s" my/hydra-seeing-is-believing/body "toggle seeing-is-believing" :exit t))))
+
+  (pretty-hydra-define my/hydra-seeing-is-believing
+    (:hint nil :color amaranth :quit-key "q" :title (with-faicon "eye" "Seeing is believing" 1 -0.05))
+    ("Action"
+      (("t" seeing-is-believing "toggle" :toggle t :exit t)
+        ("r" seeing-is-believing-run "run")
+        ("c" seeing-is-believing-clear "clear" :exit t)))))
+
+(use-package seeing-is-believing
+  :commands seeing-is-believing)
 
 ;; (use-package rbenv
 ;;   :after ruby-mode
@@ -53,6 +54,16 @@
 ;; https://melpa.org/#/rspec-mode
 ;; https://melpa.org/#/rbtagger :: code navigation
 ;; https://melpa.org/#/format-all
+
+;; (use-package robe
+;; :config
+;; (progn
+;;   (add-hook 'robe-mode-hook (lambda ()
+;; (robe-start)
+;;                               (make-local-variable 'company-backends)
+;;                               (add-to-list 'company-backends 'company-robe t)))
+;;   (add-hook 'ruby-mode-hook 'robe-mode)
+;;   ))
 
 (provide 'my-ruby)
 ;;; my-ruby.el ends here
