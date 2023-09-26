e95038c07c4cb6ac39041c2c1285b1258be76f2f
adamWithF <adam10@puremail.cyou>
2023-09-26 21:59:56 +0100

    Emacs: centaur-tabs added ignore to certain buffers

--- emacs.d/modules/my-navigation.el
+++ emacs.d/modules/my-navigation.el
@@ -740,7 +740,35 @@ point reaches the beginning or end of the buffer, stop there."
   (centaur-tabs-enable-buffer-reordering)
   (centaur-tabs-enable-buffer-alphabetical-reordering)
   (centaur-tabs-group-by-projectile-project)
-  (add-to-list 'evil-emacs-state-modes 'lsp-ui-imenu-mode))
+  (add-to-list 'evil-emacs-state-modes 'lsp-ui-imenu-mode)
+
+  (defun centaur-tabs-hide-tab (x)
+    "Do no to show buffer X in tabs."
+    (let ((name (format "%s" x)))
+      (or
+        ;; Current window is not dedicated window.
+        (window-dedicated-p (selected-window))
+
+        ;; Buffer name not match below blacklist.
+        (string-prefix-p "*epc" name)
+        (string-prefix-p "*helm" name)
+        (string-prefix-p "*Helm" name)
+        (string-prefix-p "*Compile-Log*" name)
+        (string-prefix-p "*lsp" name)
+        (string-prefix-p "*company" name)
+        (string-prefix-p "*Flycheck" name)
+        (string-prefix-p "*tramp" name)
+        (string-prefix-p " *Mini" name)
+        (string-prefix-p "*help" name)
+        (string-prefix-p "*straight" name)
+        (string-prefix-p " *temp" name)
+        (string-prefix-p "*Help" name)
+        (string-prefix-p "*mybuf" name)
+
+        ;; Is not magit buffer.
+        (and (string-prefix-p "magit" name)
+          (not (file-name-extension name)))
+        ))))
 
 ;; (use-package demap)
 
