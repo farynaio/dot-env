5e4f84362d035e02b0a72da4c87301ecf100bad9
farynaio <adam10@puremail.cyou>
2023-12-12 07:35:17 +0000

    Emacs: new abbrevs

--- emacs.d/modules/my-writing.el
+++ emacs.d/modules/my-writing.el
@@ -161,6 +161,8 @@
                     ("totaly" "totally" nil 0)
                     ("readonly" "read-only" nil 0)
 
+                    ("shopify" "Shopify" nil 0)
+                    ("sms" "SMS" nil 0)
                     ("ios" "iOS" nil 0)
                     ("android" "Android" nil 0)
                     ("google" "Google" nil 0)
@@ -241,6 +243,8 @@
 (defvar my/pl-abbrevs nil)
 (define-abbrev-table
   'my/pl-abbrevs '(
+                    ("shopify" "Shopify" nil 0)
+                    ("sms" "SMS" nil 0)
                     ("ios" "iOS" nil 0)
                     ("android" "Android" nil 0)
                     ("google" "Google" nil 0)
