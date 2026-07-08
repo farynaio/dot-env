67b3f866484a1862be9e1f960ea2b3b28cfae278
farynaio <adam10@puremail.cyou>
2026-07-09 00:30:54 +0800

    Termux + Tmux: basic setup

--- termux/bin/tmux-start.sh
+++ termux/bin/tmux-start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-tmux new-session -d -s mysession -n xfce '~/bin/xfce-start.sh' \; \
-  new-window -t mysession -n emacs 'emacs' \; \
-  attach -t mysession
+if tmux has-session -t mysession 2>/dev/null; then
+  tmux attach -t mysession
+else
+ tmux new-session -d -s mysession -n emacs fish
+ tmux new-window -t mysession -n fish fish
+ tmux attach -t mysession
 
-exec tmux attach -t mysession
+  # tmux new-session -d -s mysession -n emacs 'fish' \; new-window -t mysession -n sh 'fish' \; select-window -t mysession:1 attach -t mysession
+fi
