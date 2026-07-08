bae591693ad9253ea396d746722e5e27d34ba11d
farynaio <adam10@puremail.cyou>
2026-07-08 18:39:16 +0800

    Termux / Fish: Emacs client aliases

--- termux/config.fish
+++ termux/config.fish
@@ -16,7 +16,15 @@ alias pkgd="pkg show "
 alias pkgi="pkg install "
 alias pkgrm="pkg uninstall "
 alias pkglsu="apt list --upgradable"
-#alias xfce="termux-x11 :1 -xstartup \"dbus-launch --exit-with-session xfce4-session\""
+alias e 'my_emacs_run'
+
+function my_emacs_run
+    if isatty stdin
+        eval $EDITOR
+    else
+        eval $VISUAL
+    end
+end
 
 function fish_prompt
     printf '%s > ' (basename (pwd))
