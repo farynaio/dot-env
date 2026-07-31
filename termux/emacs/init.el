c209d0d7eef81043c67e68a05bbb7005b251149f
farynaio <adam10@puremail.cyou>
2026-07-31 18:59:49 +0700

    Termux / Emacs: easier tab-bar navigation

--- termux/emacs/init.el
+++ termux/emacs/init.el
@@ -1216,7 +1216,9 @@
   :demand t
   :straight nil
   :bind (("C-x <right>" . tab-bar-switch-to-next-tab)
-         ("C-x <left>" . tab-bar-switch-to-prev-tab))
+         ("C-x <left>" . tab-bar-switch-to-prev-tab)
+         ("C-x C-<right>" . tab-bar-switch-to-next-tab)
+         ("C-x C-<left>" . tab-bar-switch-to-prev-tab))
   :custom
   (tab-bar-auto-width t)
   ;; (tab-bar-tab-name-function #'tab-bar-tab-name-truncated)
